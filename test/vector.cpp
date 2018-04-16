1c7db3b3f63eca757a77cac30209a5bcc20f2819
Cameron Reikes <cameronreikes@gmail.com>
2018-04-15 19:42:15 -0700

    Change Vector type to Vector2

--- test/vector.cpp
+++ test/vector.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 
 int main(void) {
-  alhelp::Vector<int> myVec(5, 4);
+  alhelp::Vector2<int> myVec(5, 4);
   std::cout << "My vec: " << myVec.toString() << std::endl;
   std::cout << "Length: " << myVec.length() << std::endl;
   std::cout << "Normalized: " << myVec.normalize().toString() << std::endl;
