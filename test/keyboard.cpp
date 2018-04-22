ba2ce67c1d7495c7bd2006350363b640f5cfd3cb
Cameron Reikes <cameronreikes@gmail.com>
2018-04-21 22:10:29 -0700

    Make keyboard demo way more trippy

--- test/keyboard.cpp
+++ test/keyboard.cpp
@@ -4,16 +4,34 @@
 #include <allegro5/allegro_primitives.h>
 #include <iostream>
 
-class Square : public alhelp::Frontend {
+class FPSCounter : public alhelp::Backend {
 public:
   void run(double delta) {
     this->fpsCounter += delta;
     this->frameCounter++;
-    if (frameCounter >= this->fps) {
+    if (this->frameCounter >= this->fps) {
       std::cout << "Fps: " << this->fpsCounter << std::endl;
-      this->fpsCounter = 0;
       this->frameCounter = 0;
+      this->fpsCounter = 0;
     }
+  }
+  void setSystem(alhelp::System *newSys) { this->sys = newSys; }
+  std::string getID() { return this->myID; };
+  FPSCounter(alhelp::System *inSys) : sys(inSys) {
+    this->fps = this->sys->getFps();
+  };
+
+private:
+  alhelp::System *sys;
+  double fpsCounter = 0;
+  int frameCounter = 0;
+  double fps;
+  std::string myID = "Frame Counter";
+};
+
+class Square : public alhelp::Frontend {
+public:
+  void run(double delta) {
     if (this->sys->keyIsDown(ALLEGRO_KEY_RIGHT))
       this->pos.x += this->speed;
     if (this->sys->keyIsDown(ALLEGRO_KEY_LEFT))
@@ -22,6 +40,9 @@ public:
       this->pos.y -= this->speed;
     if (this->sys->keyIsDown(ALLEGRO_KEY_DOWN))
       this->pos.y += this->speed;
+    this->size.x += delta;
+    this->size.y += delta;
+    // std::cout << "Increasing by " << delta << "size!" << std::endl;
   }
   void setSystem(alhelp::System *newSystem) { this->sys = newSystem; };
   std::string getID() { return this->myID; };
@@ -33,17 +54,12 @@ public:
   Square(alhelp::Vector2<double> inSize, alhelp::SafeColor inColor,
          double inSpeed, alhelp::System *inSys, std::string inID)
       : pos(alhelp::Vector2<double>(0, 0)), size(inSize), myColor(inColor),
-        speed(inSpeed), sys(inSys), myID(inID) {
-    this->fps = this->sys->getFps();
-  };
+        speed(inSpeed), sys(inSys), myID(inID){};
 
 private:
   alhelp::Vector2<double> pos;
   alhelp::Vector2<double> size;
   alhelp::SafeColor myColor;
-  double fpsCounter = 0;
-  int frameCounter = 0;
-  double fps;
   const int zIndex = 1;
   double speed;
   alhelp::System *sys;
@@ -54,10 +70,11 @@ int main(int argc, char **argv) {
   alhelp::System sys(alhelp::Vector2<int>(500, 500),
                      alhelp::SafeColor(50, 50, 50), 60);
   sys.init();
+  sys.addBackend(new FPSCounter(&sys));
   for (int i = 0; i < 100; i++) {
-    sys.addFrontend(
-        new Square(alhelp::Vector2<double>(i * 5 + 10.0, i * 5 + 10.0),
-                   alhelp::SafeColor(255, 0, 0), 5, &sys, std::to_string(i)));
+    sys.addFrontend(new Square(
+        alhelp::Vector2<double>(i * 2 + 10.0, i * 2 + 10.0),
+        alhelp::SafeColor(i, i * 2, i * 3), 5, &sys, std::to_string(i)));
   }
   while (sys.getClose() == false) {
     sys.run();
